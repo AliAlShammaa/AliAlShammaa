9dd6e2adff5d30c31b60e3bc1f3a9a7dc2a32d72
AliAlShammaa <ausama625@gmail.com>
2021-01-26 23:22:18 -0500

    push

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -10,7 +10,9 @@ export default function Intro() {
     <div id="intro" className="container-fluid padding intro mt-4">
       <div className="row no-gutters " id="innerintro">
         <div className="col-0 col-md-1"> </div>
-        <div className="col-md-4 col-12 pl-5 mr-0 pr-0">
+        <div className="col-md-4 col-12 col-sm-12 pl-5 mr-0 pr-0 ">
+          {" "}
+          {/*/=text-center */}
           <div className="">
             <img
               className="picOfMe block-responsive rounded mb-4 silhouette"
@@ -20,16 +22,16 @@ export default function Intro() {
           </div>{" "}
         </div>
         {/* <div className="container-fluid">
-              <div className="row">
-                <i className="col-md-2 fa fa-code fa-4x m-3"></i>
-                <i className="col-md-2 fa fa-chrome chrome-grad fa-4x m-3"></i>
-                <i className="col-md-2 fa fa-laptop fa-4x m-3"></i>
-                <i className="col-md-2 fa fa-book fa-4x m-3"></i>
-                <i className="col-md-2 fa fa-university fa-4x m-3"></i>
-                <i className="col-md-2 fa fa-fa-sort-numeric-asc fa-4x m-3"></i>
-              </div>
-            </div> */}
-        <div className=" col-md-6  ml-0 pl-0 welcome text-white">
+          <div className="row">
+            <i className="col-md-2 fa fa-code fa-4x m-3"></i>
+            <i className="col-md-2 fa fa-chrome chrome-grad fa-4x m-3"></i>
+            <i className="col-md-2 fa fa-laptop fa-4x m-3"></i>
+            <i className="col-md-2 fa fa-book fa-4x m-3"></i>
+            <i className="col-md-2 fa fa-university fa-4x m-3"></i>
+            <i className="col-md-2 fa fa-fa-sort-numeric-asc fa-4x m-3"></i>
+          </div>
+        </div> */}
+        <div className=" col-md-6 ml-0 pl-0 welcome text-white">
           <hr />
           <h1 className="display-3 text-center">
             <span className="display-4">👋 </span>
@@ -46,65 +48,69 @@ export default function Intro() {
             </p>
           </strong>
           <div className="conatiner-fluid">
-            <div className="row">
-              <br />
-              <div className="col-md-6 col-12">
-                <p> I love programming!!</p>
-                <p>
-                  I am interested in :
-                  <br />
-                  <ul>
-                    <li className="mb-0 mt-0"> Web development</li>
-                    <li className="mb-0 mt-0"> Machine learning </li>
-                    <li> Quantum Algorithims </li>
-                  </ul>
-                </p>
-              </div>
+            <div className="col-12">
+              <div className="row">
+                <br />
+                <div className="col-md-6 col-12">
+                  <p> I love programming!!</p>
+                  <p>
+                    I am interested in :
+                    <br />
+                    <ul>
+                      <li className="mb-0 mt-0"> Web development</li>
+                      <li className="mb-0 mt-0"> Machine learning </li>
+                      <li> Quantum Algorithims </li>
+                    </ul>
+                  </p>
+                </div>
 
-              <div id="situation" className="col-md-6 col-12 exp mt-">
-                <p>
-                  {" "}
-                  I am currently @<a className="btn-outline-warning">UW CS</a>
-                </p>
-                <p>
-                  {" "}
-                  Prev worked @<a className="btn-outline-success">Atlas365</a>
-                </p>
-                <p>
-                  {" "}
-                  Interned @<a className="btn-outline-info">Litrans Lab</a>
-                </p>
+                <div id="situation" className="col-md-6 col-12 exp mt-">
+                  <p>
+                    {" "}
+                    I am currently @<a className="btn-outline-warning">UW CS</a>
+                  </p>
+                  <p>
+                    {" "}
+                    Prev worked @<a className="btn-outline-success">Atlas365</a>
+                  </p>
+                  <p>
+                    {" "}
+                    Interned @<a className="btn-outline-info">Litrans Lab</a>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="conatiner-fluid mt-4">
-            <div className="row text-center">
-              {/* <div class="btn-group" role="group" aria-label="Basic example"> */}
-              <div className="col-sm-6 col-lg-4">
-                <span className="git border border-info px-3 py-3 rounded btn-info btn-outline-dark">
+            <div className="conatiner-fluid mt-4 col-12 col-xl-10">
+              <div className="row text-center no-gutters">
+                {/* <div class="btn-group" role="group" aria-label="Basic example"> */}
+                <div className="col-12 col-md-5 col-lg-3 mb-4 mt-4 mx-0">
                   <a href="https://github.com/AliAlShammaa/">
-                    <i className="fa fa-2x fa-github"></i> GitHub{" "}
+                    <span className="git border border-info px-3 py-3 mb-1 mx-0 rounded btn-info btn-outline-dark">
+                      <i className="fa fa-2x fa-github"></i> GitHub{" "}
+                    </span>
                   </a>
-                </span>
-              </div>
+                </div>
 
-              <div className="col-sm-6 col-lg-4">
-                <span className=" linkedin border border-info px-3 py-3 mb-1 mx-0 rounded btn-info btn-outline-dark ">
+                <div className="col-12 col-md-5 col-lg-4 mb-4 mt-4 ml-3">
                   <a href="https://www.linkedin.com/in/ali-al-shammaa-508a59bb/">
-                    <i className="fa fa-2x fa-linkedin"></i> Linkedin{" "}
+                    <span className=" linkedin border border-info px-3 py-3 rounded btn-info btn-outline-dark ">
+                      <i className="fa fa-2x fa-linkedin"></i> Linkedin{" "}
+                    </span>
                   </a>
-                </span>
-              </div>
-              <br></br>
-              <div className="col-lg-4">
-                <span className="linkedin border border-info px-3 py-3 mx-0 rounded btn-info btn-outline-dark">
-                  {" "}
+                </div>
+
+                <div className="col-12 col-md-6 col-lg-4 mt-4 ml-0">
                   <a href={Resume} target="blank">
-                    <i className="fa fa-2x fa-file-pdf-o"></i> Resume/CV{" "}
+                    <span className="linkedin border border-info px-3 py-3 rounded btn-info btn-outline-dark">
+                      {" "}
+                      <i className="fa fa-2x fa-file-pdf-o"></i> Résumé{" "}
+                    </span>
                   </a>
-                </span>
+                  {/* </div> */}
+                </div>
+
+                {/* </div> */}
               </div>
-              {/* </div> */}
             </div>
           </div>
           <br /> <br /> <br />{" "}
